8b67cd65e816762e3d12d9963dda3f576dc46c6a
alex <aperera@gmail.com>
2025-02-08 15:28:31 +0100

    comit encima del primer commit remoto

---
+++ _site_10-192-74-53client.eduroam.upc.edu_Feb-06-142842-2025_conflict_parent/publications_10-192-74-53client.eduroam.upc.edu_Feb-06-142642-2025_conflict_parent/tabs.js
@@ -0,0 +1,13 @@
+function openTab(evt, decade) {
+    var i, tabcontent, tabbuttons;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tabbuttons = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tabbuttons.length; i++) {
+        tabbuttons[i].classList.remove("active");
+    }
+    document.getElementById(decade).style.display = "block";
+    evt.currentTarget.classList.add("active");
+}
